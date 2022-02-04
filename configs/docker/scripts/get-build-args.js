ba44a465c4a6e46e91302732531eed9a168f6344
Bogdan Kolesnyk <bogdan.kolesnyk@gmail.com>
2022-02-04 22:23:55 +0100

    foo bar

--- configs/docker/scripts/get-build-args.js
+++ configs/docker/scripts/get-build-args.js
@@ -1,23 +1,25 @@
 const { execSync } = require('child_process');
-const { resolve } = require('path');
+const { resolve, sep } = require('path');
 
-module.exports = (dockerFile = 'Dockerfile') => {
-    const cwd = process.cwd();
-    const packageJsonPath = resolve(cwd, 'package.json');
-    const gitRoot = resolve(execSync('git rev-parse --show-toplevel')
-      .toString()
-      .replace('\n', ''));
+module.exports = (
+  dockerfile = 'Dockerfile',
+  dockerfilesDir = 'configs/docker/dockerfiles',
+) => {
+  const cwd = process.cwd();
+  const packageJsonPath = resolve(cwd, 'package.json');
+  const gitRoot = resolve(execSync('git rev-parse --show-toplevel')
+    .toString()
+    .replace('\n', ''));
 
-    const NAME = require(packageJsonPath).name.replace('@', '');
-    const DOCKERFILE = resolve(__dirname, '../dockerfiles', dockerFile);
+  const rootDepth = cwd.split(sep).length - gitRoot.split(sep).length;
 
-    const environment = {
-      NAME: require(packageJsonPath).name.replace('@', ''),
-      CONTEXT: gitRoot,
-      DOCKERFILE: resolve(__dirname, '../dockerfiles', dockerFile),
-    }
-
-    console.log('\n\n\n\n', JSON.stringify(environment, null, 2),'\n\n\n\n')
-
-    return environment;
+  const NAME = require(packageJsonPath).name.replace('@', '');
+  const CONTEXT = [...Array(rootDepth)].map(() => '../').join('');
+  const DOCKERFILE = [CONTEXT, dockerfilesDir, '/', dockerfile].join('');
+  
+  return {
+    NAME,
+    CONTEXT,
+    DOCKERFILE,
   }
+}
