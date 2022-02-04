822558dfdc9dddb3f3ce798eec8b4013301bb124
Bogdan Kolesnyk <bogdan.kolesnyk@gmail.com>
2022-02-04 22:32:47 +0100

    foo bar

--- apps/rock/app/.env.js
+++ apps/rock/app/.env.js
@@ -1,1 +1,5 @@
-module.exports = require('../../../configs/docker/scripts').getBuildArgs();
+const ENV = require('../../../configs/docker/scripts').getBuildArgs();
+
+console.log('\n\n\n\n', ENV, '\n\n\n\n');
+
+module.exports = ENV;
