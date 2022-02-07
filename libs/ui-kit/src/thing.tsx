9bf953f18b3729e28f016fa712c9f097ad1bcfba
Bogdan Kolesnyk <b12k@users.noreply.github.com>
2022-02-07 11:07:49 +0100

    Update thing.tsx

--- libs/ui-kit/src/thing.tsx
+++ libs/ui-kit/src/thing.tsx
@@ -11,5 +11,5 @@ export interface Props extends HTMLAttributes<HTMLDivElement> {
  * A custom Thing component. Neat!
  */
 export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries wildberries!!! boom!`}</div>;
+  return <div>{children || `the snozzberries taste like snozzberries wildberries! ;lzsjfhgskjdhfglsdfhkgs`}</div>;
 };
